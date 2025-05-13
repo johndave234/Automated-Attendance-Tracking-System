9f15c7404c36c49a69a38fc0f0311b50a76c684f
johndave234 <johndavelazaga18@gmail.com>
2025-05-13 17:46:10 +0800

    Delete 5. Implementations/backend directory

--- 5. Implementations/backend/config/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    mongoURI: 'mongodb+srv://tamingdan:Azreh2415@cluster0.vh9qmx2.mongodb.net/JDGwapo?retryWrites=true&w=majority&appName=Cluster0',
-    jwtSecret: 'your-secret-key',
-    port: process.env.PORT || 5000,
-};
-
-module.exports = config; 
